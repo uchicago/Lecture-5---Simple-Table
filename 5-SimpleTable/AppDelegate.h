67e3c3d4d7d6c692fe77e77c8c19c7bf59fb793a
T. Andrew Binkowski <abinkowski@uchicago.edu>
2011-10-24 00:07:35 -0500

    Initial Commit

---
+++ 5-SimpleTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  5-SimpleTable
+//
+//  Created by T. Andrew Binkowski on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
